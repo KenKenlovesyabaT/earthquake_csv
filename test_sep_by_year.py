55f34624a9539d5fc3ebee9c8479b91c985c4ebf
KenKenlovesyabaT <kntrngy@gmail.com>
2022-07-14 20:57:47 +0900

    sep_by_year

--- test_sep_by_year.py
+++ test_sep_by_year.py
@@ -26,19 +26,22 @@ def mag(data):
 
 
 for year in ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"]:
+#for year in ["2019"]:
     rf = open("h"+year, "r", encoding="utf-8")
     #with open("sample.csv", "w", encoding="utf-8") as f:
     #with open("sample_m3.csv", "w", encoding="utf-8") as f:
     #with open("sample_m4.csv", "w", encoding="utf-8") as f:
-    with open(year+"_sample_m5.csv", "w", encoding="utf-8") as f:
-        header = ['ID', 'date', 'lat', 'lng', 'magnitude']
+    with open(year+"_sample_sep_by_year_m6.csv", "w", encoding="utf-8") as f:
+        header = ['ID', 'year', 'month', 'lat', 'lng', 'magnitude']
         writer = csv.DictWriter(f, fieldnames=header)
         writer.writeheader()
 
         datalist = rf.readlines()
         eq_id = 0
         for data in datalist:
-            date = data[1:5] + '-' + data[5:7] + '-' + data[7:9]
+            #date = data[1:5] + '-' + data[5:7] + '-' + data[7:9]
+            year = data[1:5]
+            month = data[5:7]
             #lat_degree = int(data[21:24])
             #lat_minute = int(data[24:26]) + (float(data[26:28]) / 100)
             latitude = s_to_d (data[21:24], data[24:28])
@@ -48,7 +51,7 @@ for year in ["2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "20
             magnitude = mag(data[52:54])
             #if magnitude >= 4:
             if magnitude >= 5:
-                writer.writerow({'ID': eq_id, 'date': date, 'lat': latitude, 'lng': longitude, 'magnitude': magnitude})
+                writer.writerow({'ID': eq_id, 'year': year, 'month': month, 'lat': latitude, 'lng': longitude, 'magnitude': magnitude})
                 eq_id += 1
 
 
